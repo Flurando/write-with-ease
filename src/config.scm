28c8f8811c90a9cab71507b2e593062ab304eb20
Flurando <2024110144@mails.szu.edu.cn>
2024-10-19 18:55:24 +0800

    write a io module and some minor tweats like adding some words in STRUCTURE

--- src/config.scm
+++ src/config.scm
@@ -8,7 +8,10 @@
 ;;;automatic config-pairs key generating are preferred.
 (define (update-config)
 	(display "running update-config...\n")
-	(system (format #f "sed -i -e 's/(current-file-name.*)/(current-file-name . \"~a\")/' ./config.scm" (assq-ref config-pairs 'current-file-name)
+	(let ((new-file-name (assq-ref config-pairs 'current-file-name)))
+		(system
+			(format #f "sed -i -e 's/(current-file-name . \\\".*\\\")/(current-file-name . \\\"~a\\\")/' ./config.scm" new-file-name
+			)
 		)
 	)
 )
